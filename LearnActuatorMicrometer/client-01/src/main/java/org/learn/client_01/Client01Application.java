19a6df5d31eca00ebb77427d1c49e7667fddcc00
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-12-31 00:47:34 +0530

    .

--- LearnActuatorMicrometer/client-01/src/main/java/org/learn/client_01/Client01Application.java
+++ LearnActuatorMicrometer/client-01/src/main/java/org/learn/client_01/Client01Application.java
@@ -3,11 +3,14 @@ package org.learn.client_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Client01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Client01Application.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(Client01Application.class, args);
+    }
 
 }
