2ab86a6085365bade577c9a7e193b2c55ace328d
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-12-31 00:40:19 +0530

    .

--- LearnActuatorMicrometer/gateway/src/main/java/org/learn/gateway/GatewayApplication.java
+++ LearnActuatorMicrometer/gateway/src/main/java/org/learn/gateway/GatewayApplication.java
@@ -3,11 +3,14 @@ package org.learn.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
 }
