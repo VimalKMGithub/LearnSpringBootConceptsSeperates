92398852f0ff5a908026090aebf337aa28153482
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2026-01-02 21:18:20 +0530

    .

--- EventDriven/service-2/src/main/java/com/example/service_2/UserProfileRepository.java
+++ EventDriven/service-2/src/main/java/com/example/service_2/UserProfileRepository.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    // Find profile by the external Auth User ID
     Optional<UserProfile> findByUserId(UUID userId);
 
+    // For deleting when a user is removed
     void deleteByUserId(UUID userId);
 }
